4aacbb4b258264d2c74f85261e3ac7df0290eb22
Jihee <dhkdnun@naver.com>
2021-07-25 16:06:29 +0900

    첫번째 커밋

---
+++ src/main/java/com/jihee/msub/MsubApplication.java
@@ -0,0 +1,13 @@
+package com.jihee.msub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsubApplication.class, args);
+	}
+
+}
