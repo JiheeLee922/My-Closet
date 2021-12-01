a56a61efe7da6e6efbc253bf8b4b23d825d241f1
Jihee Lee <dhkdnun@naver.com>
2021-12-01 16:29:57 +0900

    [feat] Querydsl 적용, 페이징,검색 처리

--- src/main/java/com/jihee/msub/board/domain/entity/BoardEntity.java
+++ src/main/java/com/jihee/msub/board/domain/entity/BoardEntity.java
@@ -2,19 +2,25 @@ package com.jihee.msub.board.domain.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.jihee.msub.member.domain.entity.MemberEntity;
 
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(exclude = "member")
 @Table(name="board")
 public class BoardEntity extends TimeEntity{
 	@Id
@@ -29,7 +35,10 @@ public class BoardEntity extends TimeEntity{
 
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
-
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    private MemberEntity member;
+    
     @Builder
     public BoardEntity(Long id, String title, String content, String writer) {
         this.id = id;
